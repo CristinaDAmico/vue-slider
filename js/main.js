b11b451aa3b560721df6788346e71b5ee59474d8
CristinaDAmico <crydamico94@gmail.com>
2021-04-21 20:11:21 +0200

    completed

--- js/main.js
+++ js/main.js
@@ -13,6 +13,12 @@ const app = new Vue({
             './immagini/img4.jpg',
         ],
         indexPhoto: 0,
+        intervalID: 0,
+    },
+    created() {
+        // Create l'istanza vue
+        this.startLoop();
+
     },
     methods: {
         prevPhoto() {
@@ -29,6 +35,19 @@ const app = new Vue({
             if(this.indexPhoto > (this.photos.length - 1)) {
                 this.indexPhoto = 0;
             }
+        },
+        setPhoto(index) {
+            console.log(index);
+            this.indexPhoto = index;
+        },
+        startLoop() {
+            this.intervalID = setInterval(() => {
+                this.nextPhoto();
+            }, 3000);
+        },
+        stopLoop () {
+            clearInterval(this.intervalID);
         }
     }
 });
+
